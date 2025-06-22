2d1bcc2f23d301e9b45736c9d9dd4c6eff66767b
toru-sakairi <tdu23fi044@gmail.com>
2025-06-22 14:46:58 +0900

    settings.py changes to CSS&Media Folder

--- zerocrew/zerocrew/settings.py
+++ zerocrew/zerocrew/settings.py
@@ -20,10 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ▼▼▼ .envファイルを読み込む処理を追加 ▼▼▼
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -98,6 +94,13 @@ if DEBUG:
             'PORT': '5432',              # PostgreSQLのデフォルトのポート番号
         }
     }
+    STATIC_URL = 'static/'
+
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 # 本番環境の設定
 else:
     DATABASES = {
@@ -166,11 +169,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -180,9 +178,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # ログイン/ログアウトに関するURL設定
 LOGIN_URL = 'users:login'
