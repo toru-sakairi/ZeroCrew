838bec36e6205b82760920f041f3d9b24c93c0f1
toru-sakairi <tdu23fi044@gmail.com>
2025-06-22 14:51:20 +0900

    2025/06/22 いったん全部pushします

--- zerocrew/zerocrew/urls.py
+++ zerocrew/zerocrew/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# 2025/06/22 projects.urlのやつのパスをサーバー側のやつに合わせた。エラー出たら治して
 urlpatterns = [
+    path('', include('projects.urls')),
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
-    path('projects/', include("projects.urls")),
+    # path('projects/', include("projects.urls")),
 ]
 
 if settings.DEBUG:
