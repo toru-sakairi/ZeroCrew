f349795dd9e3a3304985d1f9eac525e23baf8b8e
toru-sakairi <tdu23fi044@gmail.com>
2025-06-29 21:19:43 +0900

    add privacy_policy & change terms UI

--- zerocrew/pages/urls.py
+++ zerocrew/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TermsOfServiceView, PrivacyPolicyView, Landing_AboutView
+from .views import TermsOfServiceView, Privacy_PolicyView, Landing_AboutView
 
 # アプリケーションの名前空間を設定
 app_name = 'pages'
@@ -8,7 +8,7 @@ urlpatterns = [
     # /terms/ というURLで利用規約ページを表示
     path('terms/', TermsOfServiceView.as_view(), name='terms'),
     # /privacy/ というURLでプライバシーポリシーページを表示
-    path('privacy/', PrivacyPolicyView.as_view(), name='privacy'),
+    path('privacy_policy/', Privacy_PolicyView.as_view(), name='privacy_policy'),
     # /landing_about/ というURLでランディングページ＆aboutについてのページを表示
     path('landing_about/', Landing_AboutView.as_view(), name='landing_about'),
 ]
--- zerocrew/pages/views.py
+++ zerocrew/pages/views.py
@@ -6,17 +6,16 @@ class TermsOfServiceView(TemplateView):
     """
     # templates/pages/ ディレクトリ内の `terms_of_service.html` を指定
     template_name = "pages/terms_of_service.html"
-
-class PrivacyPolicyView(TemplateView):
-    """
-    プライバシーポリシーページを表示するためのビュー
-    """
-    # 今後作成するプライバシーポリシーのテンプレートを指定
-    template_name = "pages/privacy_policy.html"
     
 class Landing_AboutView(TemplateView):
     """
     ランディングページ＆Aboutのページを表示するためのビュー
     """
     template_name = "pages/landing_about.html"
+    
+class Privacy_PolicyView(TemplateView):
+    """
+    プライバシーポリシーのページを表示するためのビュー
+    """
+    template_name = "pages/privacy_policy.html"
 
